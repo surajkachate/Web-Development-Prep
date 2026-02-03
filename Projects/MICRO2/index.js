df19eeb173fda91950034a5fdc0e8e90e0146c98
surajkachate <surajkachate021@gmail.com>
2026-02-03 10:40:35 +0530

    update changes

--- Projects/MICRO2/index.js
+++ Projects/MICRO2/index.js
@@ -2,10 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes")
+
+// BASE URL: http://localhost:3000
 
 dotenv.config();
 connectDB();
 const app = express();
+
+// middleware
+app.use(express.json());
+
+// Request: POST
+// URL: http://localhost:3000/api/auth/register
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on port ${process.env.PORT}`);
